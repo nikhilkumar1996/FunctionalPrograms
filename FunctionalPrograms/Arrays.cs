7e766efd91466ba5159b14390c852d7e858ac48a
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-10 01:30:44 +0530

    [Nikhil Kumar]:2DArray Program Added

---
+++ FunctionalPrograms/Arrays.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class Arrays
+    {
+        public void TwoDimensionalArray()
+        {
+            Console.WriteLine("Enter no of rows");
+            int r = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter no of columns");
+            int c = Convert.ToInt32(Console.ReadLine());
+
+            //creating Array-1d
+            Console.WriteLine("Enter values:");
+            int[] onedimensionalarray = new int[r * c];
+            for (int i = 0; i < onedimensionalarray.Length; i++)
+            {
+                onedimensionalarray[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            //creating Array-2d
+            int index = 0;
+            int[,] twodimensionalarray = new int[r, c];
+            for (int j = 0; j < r; j++)
+            {
+                for (int k = 0; k < c; k++)
+                {
+                    twodimensionalarray[j, k] = onedimensionalarray[index];
+                    index++;
+
+                }
+
+            }
+
+            Console.WriteLine("2d array elements");
+            foreach (int item in twodimensionalarray)
+            {
+                Console.WriteLine(item + " ");
+            }
+
+
+
+        }
+    }
+}
--- FunctionalPrograms/Program.cs
+++ FunctionalPrograms/Program.cs
@@ -11,6 +11,30 @@ namespace FunctionalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Functional Programs");
+            Console.WriteLine(  "1)2D Array" +
+                              "\n2)Exit");
+            
+            bool flag = true;
+            while (flag)
+            {
+                Console.WriteLine("Enter Choice");
+                int n = Convert.ToInt32(Console.ReadLine());
+                switch (n)
+                {
+                    case 1:
+                        Arrays arrays = new Arrays();
+                        arrays.TwoDimensionalArray();
+                        break;
+
+                    case 2:
+                        flag= false;
+                        break;
+
+                    default:
+                        Console.WriteLine("INvalid Option");
+                        break;
+                }
+            }
         }
     }
 }
