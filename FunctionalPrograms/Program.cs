490d87291ca148ff5672364e31deed9f0743b1c6
nikhilkumar1996 <nikhilkumar1097@gmail.com>
2022-02-10 01:22:28 +0530

    [Nikhil Kumar]:Welcome to Functional Programs

---
+++ FunctionalPrograms/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunctionalPrograms
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Functional Programs");
+        }
+    }
+}
